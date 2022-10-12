cc807f805f52d90b274656c9bb592d8b5e2f97b3
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2022-10-12 10:18:38 +0100

    feat: use registerActionMiddleware to setup Inertia policy

--- index.js
+++ index.js
@@ -19,13 +19,76 @@ module.exports = function defineInertiaHook(sails) {
   let sharedProps = {}
   let sharedViewData = {}
   let rootView = 'app'
+
+  const inertiaMiddleware = (req, res, next) => {
+    hook.render = function (component, props = {}, viewData = {}) {
+      const allProps = {
+        ...sharedProps,
+        ...props,
+      }
+
+      const allViewData = {
+        ...sharedViewData,
+        ...viewData,
+      }
+
+      let url = req.url || req.originalUrl
+      const assetVersion = sails.config.inertia.version
+      const currentVersion =
+        typeof assetVersion == 'function' ? assetVersion() : assetVersion
+
+      const page = {
+        component,
+        version: currentVersion,
+        props: allProps,
+        url,
+      }
+
+      // Implements inertia partial reload. See https://inertiajs.com/partial-reload
+      if (req.get(PARTIAL_DATA) && req.get(PARTIAL_COMPONENT) === component) {
+        const only = req.get(PARTIAL_DATA).split(',')
+        page.props = only.length ? getPartialData(props, only) : page.props
+      }
+
+      const queryParams = req.query
+      if (req.method == 'GET' && Object.keys(queryParams).length) {
+        // Keep original request query params
+        url += `?${encode(queryParams)}`
+      }
+
+      // Implements inertia requests
+      if (isInertiaRequest(req)) {
+        return res.status(200).json(page)
+      }
+
+      // Implements full page reload
+      return sails.hooks.views.render(rootView, {
+        page,
+        viewData: allViewData,
+      })
+    }
+    hook.location = function (url = req.headers['referer']) {
+      const statusCode = ['PUT', 'PATCH', 'DELETE'].includes(req.method)
+        ? 303
+        : 409
+      res.status(statusCode)
+      res.set('X-Inertia-Location', url)
+    }
+
+    // Set Inertia headers
+    if (isInertiaRequest(req)) {
+      res.set(INERTIA, true)
+      res.set('Vary', 'Accept')
+    }
+    return next()
+  }
   return {
     defaults: {
       inertia: {
         version: 1,
       },
     },
-    initialize: async function (cb) {
+    initialize: async function () {
       hook = this
       sails.inertia = hook
 
@@ -36,7 +99,8 @@ module.exports = function defineInertiaHook(sails) {
 
       hook.share('errors', {})
 
-      return cb()
+      // Register Inertia middleware
+      sails.registerActionMiddleware(inertiaMiddleware, '*')
     },
 
     share: (key, value = null) => (sharedProps[key] = value),
@@ -54,82 +118,5 @@ module.exports = function defineInertiaHook(sails) {
     setRootView: (newRootView) => (rootView = newRootView),
 
     getRootView: () => rootView,
-
-    routes: {
-      before: {
-        'GET /*': {
-          skipAssets: true,
-          fn: function (req, res, next) {
-            hook.render = function (component, props = {}, viewData = {}) {
-              const allProps = {
-                ...sharedProps,
-                ...props,
-              }
-
-              const allViewData = {
-                ...sharedViewData,
-                ...viewData,
-              }
-
-              let url = req.url || req.originalUrl
-              const assetVersion = sails.config.inertia.version
-              const currentVersion =
-                typeof assetVersion == 'function'
-                  ? assetVersion()
-                  : assetVersion
-
-              const page = {
-                component,
-                version: currentVersion,
-                props: allProps,
-                url,
-              }
-
-              // Implements inertia partial reload. See https://inertiajs.com/partial-reload
-              if (
-                req.get(PARTIAL_DATA) &&
-                req.get(PARTIAL_COMPONENT) === component
-              ) {
-                const only = req.get(PARTIAL_DATA).split(',')
-                page.props = only.length
-                  ? getPartialData(props, only)
-                  : page.props
-              }
-
-              const queryParams = req.query
-              if (req.method == 'GET' && Object.keys(queryParams).length) {
-                // Keep original request query params
-                url += `?${encode(queryParams)}`
-              }
-
-              // Implements inertia requests
-              if (isInertiaRequest(req)) {
-                return res.status(200).json(page)
-              }
-
-              // Implements full page reload
-              return sails.hooks.views.render(rootView, {
-                page,
-                viewData: allViewData,
-              })
-            }
-            hook.location = function (url = req.headers['referer']) {
-              const statusCode = ['PUT', 'PATCH', 'DELETE'].includes(req.method)
-                ? 303
-                : 409
-              res.status(statusCode)
-              res.set('X-Inertia-Location', url)
-            }
-
-            // Set Inertia headers
-            if (isInertiaRequest(req)) {
-              res.set(INERTIA, true)
-              res.set('Vary', 'Accept')
-            }
-            return next()
-          },
-        },
-      },
-    },
   }
 }
